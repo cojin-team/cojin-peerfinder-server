7fa6ed748676b8575eb1f9e1dbad92d6f088878a
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-06-30 12:00:41 +0200

    Create admin.test.js

---
+++ tests/admin.test.js
@@ -0,0 +1,68 @@
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var fetch = require('node-fetch').default
+
+// Setup Sandbox
+beforeAll(() => {
+    // .env simulation
+    process.env.dbpath = 'dbtest.log'
+    process.env.auth = 'test'
+    
+    // database simulation
+    fs.writeFileSync(process.env.dbpath, '{"peers": ["google.com:80", "google.com:443"]}')
+
+    // mount app
+    app.use('/',
+        require('../src/routes/admin.js')
+    )
+    clearInterval(process.env.dbsaver) // stop garbage collector
+    process.server = app.listen(8989)
+})
+var url = 'http://localhost:8989'
+
+test('/allPeers endpoint test', async () => {
+    // Request
+    var response = await fetch(url + '/allPeers', {
+        method: "post",
+        body: JSON.stringify({'auth': process.env.auth}),
+        headers: {"Content-Type": "application/json"}
+    })
+    // Check Response
+    response.json((peers) => {
+        expect(peers).toBe(["google.com:80", "google.com:443"])
+    })
+})
+
+test('/deletePeers endpoint test', async () => {
+    // Requests
+    var delResponse = await fetch(url + '/deletePeers', {
+        method: "post",
+        body: JSON.stringify({
+            'peers': ['google.com:443'],
+            'auth': process.env.auth
+        }),
+        headers: {"Content-Type": "application/json"}
+    })
+
+    var checkResponse = await fetch(url + '/allPeers', {
+        method: "post",
+        body: JSON.stringify({'auth': process.env.auth}),
+        headers: {"Content-Type": "application/json"}
+    })
+
+    // Check Responses
+    delResponse.text((text) => {
+        expect(text).toBe('OK')
+    })
+
+    checkResponse.json((peers) => {
+        expect(peers).toBe(['google.com:80'])
+    })
+})
+
+// Clear Sandbox
+afterAll(() => {
+    process.server.close()
+    fs.rmSync(process.env.dbpath)
+})
