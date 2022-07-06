3ab5cede72b7d6a466b3f87b40f8ca32a0a34e62
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-07-06 19:42:19 +0200

    add default options

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express')
 var app = express()
 
-if (!process.env.dbpath) require('dotenv').config()
+// Load custom options
+require('dotenv').config()
 
+// Set default options
+if (!process.env.dbpath) process.env.dbpath = 'db.json'
+if (!process.env.dbwriteInterval) process.env.dbwriteInterval = '30'
+if (!process.env.auth) process.env.auth = 'admin'
+
+// Mount user routes
 app.use('/',
     require('./src/routes/api.js')
 )
 
+// Mount admin routes
 app.use('/admin',
     require('./src/routes/admin.js')
 )
